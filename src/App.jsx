eefd6286252eec0ee822cb8e3d8fae9e6ad1ec04
AlfonsoG-M <alfonsogarciamansilla@gmail.com>
2023-04-18 10:18:57 -0300

    Subiendo rutas y navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
-
+import { Route, BrowserRouter, Routes } from "react-router-dom";
+import Navbar from "./components/layout/Navbar/Navbar";
 function App() {
-
   return (
-    <div className="App">
-      
-    </div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Navbar/>}>
+          <Route path="/" element={<h1>Home</h1>} />
+          <Route path="/quienes-somos" element={<h1>quienes-somos</h1>} />
+          <Route path="/contacto" element={<h1>contacto</h1>} />
+          <Route path="/horarios" element={<h1>horarios</h1>} />
+          <Route path="/lectio" element={<h1>lectio</h1>} />
+          <Route path="/novena" element={<h1>novena</h1>} />
+          <Route path="/bautismo" element={<h1>bautismo</h1>} />
+          <Route path="/matrimonio" element={<h1>matrimonio</h1>} />
+          <Route path="/reconciliacion" element={<h1>reconciliacion</h1>} />
+          <Route path="/uncion" element={<h1>uncion</h1>} />
+          <Route path="/talleres" element={<h1>talleres</h1>} />
+          <Route
+            path="/formacion-cristiana"
+            element={<h1>formacion-cristiana</h1>}
+          />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
