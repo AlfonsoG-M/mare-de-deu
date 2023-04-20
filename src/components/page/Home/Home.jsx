99a6c4750840bc0e9d4b4bb317ce9fa4a792ae58
AlfonsoG-M <alfonsogarciamansilla@gmail.com>
2023-04-20 16:14:49 -0300

    Subo componente matromonio

--- src/components/page/Home/Home.jsx
+++ src/components/page/Home/Home.jsx
@@ -1,9 +1,11 @@
 import { Typography } from '@mui/material'
 import React from 'react'
+import './home.css'
 
 const Home = () => {
   return (
     <div>
+      <img className='homeImg' src="https://res.cloudinary.com/dkwjizhdg/image/upload/v1681989836/Mare%20de%20deu/WhatsApp_Image_2023-04-18_at_08.48.07_iisjlj.jpg" alt="" />
         <Typography variant='h1' color={"secondary"}>Home</Typography>
     </div>
   )
--- src/components/page/Matrimonio/Matrimonio.jsx
+++ src/components/page/Matrimonio/Matrimonio.jsx
@@ -1,12 +1,210 @@
-import { Typography } from '@mui/material'
-import React from 'react'
+import { Typography, TextField, Button } from "@mui/material";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import React from "react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import './matrimonio.css'
 
 const Matrimonio = () => {
+  const { handleChange, handleSubmit, values, errors, setFieldValue } =
+    useFormik({
+      initialValues: {
+        nombre: "",
+        documento: "",
+        domicilio: "",
+        telefono: "",
+        email: "",
+        fecha: "",
+        nombreNovio:"",
+        documentoNovio:"",
+        nombreNovia:"",
+        documentoNovia: "",
+      },
+      onSubmit: (data) => {
+        const userData = {
+          nombre: data.nombre,
+          documento: data.documento,
+          domicilio: data.domicilio,
+          telefono: data.telefono,
+          email: data.email,
+          fecha: data.fecha.$d?.toLocaleDateString(),
+          nombreNovio: data.nombreNovio,
+          documentoNovio: data.documentoNovio,
+          nombreNovia: data.nombreNovia,
+          documentoNovia: data.documentoNovia,
+        };
+        console.log(userData);
+      },
+      validationSchema: Yup.object().shape({
+        nombre: Yup.string().required("Este campo es obligatorio"),
+        documento: Yup.string().required("Este campo es obligatorio"),
+        domicilio: Yup.string().required("Este campo es obligatorio"),
+        telefono: Yup.number("Sólo debe ingresar números").required(
+          "Este campo es obligatorio"
+        ),
+        email: Yup.string()
+          .email("Ingrese un email valido")
+          .required("Este campo es obligatorio"),
+        nombreNovio: Yup.string().required("Este campo es obligatorio"),
+        documentoNovio: Yup.string().required("Este campo es obligatorio"),
+        nombreNovia: Yup.string().required("Este campo es obligatorio"),
+        documentoNovia: Yup.string().required("Este campo es obligatorio"),
+      }),
+
+      validateOnChange: false,
+    });
+
   return (
     <div>
-        <Typography variant='h2'>Matrimonio</Typography>
+      <div className="divHeaderMatrimonio">
+        <img
+          src="https://res.cloudinary.com/dkwjizhdg/image/upload/v1682017354/Mare%20de%20deu/matrimonio_pmagpa.jpg"
+          alt=""
+        />
+        <Typography variant="h1" className="h1Matromonio" color="secondary.main">
+          Matromonio
+        </Typography>
+      </div>
+      <Typography variant="h6" className="formularioH6" color="secondary.main">
+        Formulario a completar para coordinar un baustimo
+      </Typography>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <Typography variant="h7" color="secondary.main">
+            Datos de contacto
+          </Typography>
+          <div className="dataDiv">
+            <TextField
+              id="outlined-basic"
+              label="Nombre y Apellido"
+              name="nombre"
+              variant="outlined"
+              value={values.nombre}
+              onChange={handleChange}
+              error={errors.nombre ? true : false}
+              helperText={errors.nombre}
+            />
+            <TextField
+              id="outlined-basic"
+              label="Documento"
+              name="documento"
+              variant="outlined"
+              value={values.documento}
+              onChange={handleChange}
+              error={errors.documento ? true : false}
+              helperText={errors.documento}
+            />
+          </div>
+          <div className="dataDiv">
+            <TextField
+              id="outlined-basic"
+              label="Domicilio"
+              name="domicilio"
+              variant="outlined"
+              value={values.domicilio}
+              onChange={handleChange}
+              error={errors.domicilio ? true : false}
+              helperText={errors.domicilio}
+            />
+          </div>
+          <div className="dataDiv">
+            <TextField
+              id="outlined-basic"
+              label="Teléfono"
+              name="telefono"
+              variant="outlined"
+              value={values.telefono}
+              onChange={handleChange}
+              error={errors.telefono ? true : false}
+              helperText={errors.telefono}
+            />
+            <TextField
+              id="outlined-basic"
+              label="e-mail"
+              name="email"
+              variant="outlined"
+              value={values.email}
+              onChange={handleChange}
+              error={errors.email ? true : false}
+              helperText={errors.email}
+            />
+          </div>
+          <div className="dataDiv">
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+              <DatePicker
+                label="Fecha casamiento"
+                name="fecha"
+                onChange={(e) => {
+                  setFieldValue("fecha", e);
+                }}
+                error={errors.fecha ? true : false}
+                helperText={errors.fecha}
+              />
+            </LocalizationProvider>
+          </div>
+        </div>
+        <div>
+        <Typography variant="h7" color="secondary.main">
+            Datos del novio
+          </Typography>
+          <div className="dataDiv">
+            <TextField
+              id="outlined-basic"
+              label="Nombre y Apellido del novio"
+              name="nombreNovio"
+              variant="outlined"
+              value={values.nombreNovio}
+              onChange={handleChange}
+              error={errors.nombreNovio ? true : false}
+              helperText={errors.nombreNovio}
+            />
+            <TextField
+              id="outlined-basic"
+              label="Documento del novio"
+              name="documentoNovio"
+              variant="outlined"
+              value={values.dodocumentoNovio}
+              onChange={handleChange}
+              error={errors.documentoNovio ? true : false}
+              helperText={errors.documentoNovio}
+            />
+          </div>
+        </div>
+        <div>
+        <Typography variant="h7" color="secondary.main">
+            Datos de la novia
+          </Typography>
+          <div className="dataDiv">
+            <TextField
+              id="outlined-basic"
+              label="Nombre de la novia"
+              name="nombreNovia"
+              variant="outlined"
+              value={values.nombreNovia}
+              onChange={handleChange}
+              error={errors.nombreNovia ? true : false}
+              helperText={errors.nombreNovia}
+            />
+            <TextField
+              id="outlined-basic"
+              label="Documento de la novia"
+              name="documentoNovia"
+              variant="outlined"
+              value={values.documentoNovia}
+              onChange={handleChange}
+              error={errors.documentoNovia ? true : false}
+              helperText={errors.documentoNovia}
+            />
+          </div>
+        </div>
+        <Button variant="contained" type="submit" className="submitButton">
+          Enviar
+        </Button>
+      </form>
     </div>
-  )
+  );
 }
 
 export default Matrimonio
