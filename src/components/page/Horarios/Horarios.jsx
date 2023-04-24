545307aaed0e3295b2873c65b39ae559c6d9f8b2
AlfonsoG-M <alfonsogarciamansilla@gmail.com>
2023-04-24 09:38:08 -0300

    Subiendo archivo vercel.json

--- src/components/page/Horarios/Horarios.jsx
+++ src/components/page/Horarios/Horarios.jsx
@@ -4,7 +4,7 @@ import './horarios.css'
 const Horarios = () => {
   return (
     <div>
-        <img class="horariosImg" src="https://res.cloudinary.com/dkwjizhdg/image/upload/v1681989819/Mare%20de%20deu/WhatsApp_Image_2023-04-20_at_08.17.23_ksgtex.jpg" alt="" />
+        <img className="horariosImg" src="https://res.cloudinary.com/dkwjizhdg/image/upload/v1681989819/Mare%20de%20deu/WhatsApp_Image_2023-04-20_at_08.17.23_ksgtex.jpg" alt="" />
     </div>
   )
 }
