20286a39050a7fe5ad68c14d7f7d26c465f7f796
AlfonsoG-M <alfonsogarciamansilla@gmail.com>
2023-04-25 09:17:07 -0300

    Estilos

--- src/components/page/Matrimonio/Matrimonio.jsx
+++ src/components/page/Matrimonio/Matrimonio.jsx
@@ -68,7 +68,7 @@ const Matrimonio = () => {
 
   return (
     <div>
-      <Grid container>
+      <Grid container >
         <Grid item xs={12} md={4}>
           <img
             src="https://res.cloudinary.com/dkwjizhdg/image/upload/v1682024121/Mare%20de%20deu/matrimonio1_cfjv3q.jpg"
@@ -113,14 +113,15 @@ const Matrimonio = () => {
           </Box>
         </Grid>
       </Grid>
-      <Typography variant="h6" className="formularioH6" color="secondary.main" sx={{fontWeight:"700"}}>
+      <Typography variant="h6" className="formularioH6" color="secondary.main" mb={1} mt={2} fontWeight={700} >
         Formulario a completar para coordinar un casamiento
       </Typography>
-      <form onSubmit={handleSubmit}>
-        <Typography variant="h7" color="secondary.main">
+      <form onSubmit={handleSubmit} style={{border:"1px solid #BD8F35", padding:"10px"}}>
+        <Typography variant="h7" color="secondary.main" bgcolor={"red"} sx={{marginBottom:"50px !important"}}>
+          
           Datos de contacto
         </Typography>
-        <Grid container spacing={2}>
+        <Grid container spacing={2} >
           <Grid item xs={12} md={6} lg={4}>
             <TextField
               id="outlined-basic"
@@ -201,7 +202,7 @@ const Matrimonio = () => {
             </LocalizationProvider>
           </Grid>
         </Grid>
-        <Typography variant="h7" color="secondary.main">
+        <Typography variant="h7" color="secondary.main" mt={4}>
           Datos del novio
         </Typography>
         <Grid container spacing={2}>
